3229ec990108c4f966587ca752fa48ad1768e81f
Pedro Antonio Labrador Muñoz <pedroantonio.labrador@avanzit.com>
2019-11-15 13:03:00 +0100

    create user witout mail activation

--- src/main/java/com/jhipster/demo/blog/service/UserService.java
+++ src/main/java/com/jhipster/demo/blog/service/UserService.java
@@ -109,10 +109,12 @@ public class UserService {
         newUser.setEmail(userDTO.getEmail().toLowerCase());
         newUser.setImageUrl(userDTO.getImageUrl());
         newUser.setLangKey(userDTO.getLangKey());
-        // new user is not active
-        newUser.setActivated(false);
-        // new user gets registration key
-        newUser.setActivationKey(RandomUtil.generateActivationKey());
+        // // new user is not active
+        // newUser.setActivated(false);
+        // // new user gets registration key
+        // newUser.setActivationKey(RandomUtil.generateActivationKey());
+        newUser.setActivated(true);
+        newUser.setActivationKey(null);
         Set<Authority> authorities = new HashSet<>();
         authorityRepository.findById(AuthoritiesConstants.USER).ifPresent(authorities::add);
         newUser.setAuthorities(authorities);
