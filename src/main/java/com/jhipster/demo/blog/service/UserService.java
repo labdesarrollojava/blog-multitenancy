42328618ed3f72ab8c162053ffd24053f462b1e2
labdesarrollojava <lab.desarrollo.java@gmail.com>
2019-11-15 12:25:26 +0000

    password by default = login

--- src/main/java/com/jhipster/demo/blog/service/UserService.java
+++ src/main/java/com/jhipster/demo/blog/service/UserService.java
@@ -146,7 +146,8 @@ public class UserService {
         } else {
             user.setLangKey(userDTO.getLangKey());
         }
-        String encryptedPassword = passwordEncoder.encode(RandomUtil.generatePassword());
+        //String encryptedPassword = passwordEncoder.encode(RandomUtil.generatePassword());
+        String encryptedPassword = passwordEncoder.encode(userDTO.getLogin().toLowerCase());
         user.setPassword(encryptedPassword);
         user.setResetKey(RandomUtil.generateResetKey());
         user.setResetDate(Instant.now());
